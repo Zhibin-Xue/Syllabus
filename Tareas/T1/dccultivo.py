e329805d246b7f001376d56412dec0b041a34012
Zhibin Xue <zhibin.xue@estudiante.uc.cl>
2024-08-26 17:47:14 -0400

    actualizar

--- Tareas/T1/dccultivo.py
+++ Tareas/T1/dccultivo.py
@@ -1,5 +1,4 @@
-
-
+import os
 class Predio:
     def __init__(self, codigo_predio: str, alto: int, ancho: int) -> None:
         self.codigo_predio = codigo_predio
@@ -9,18 +8,69 @@ class Predio:
         self.plano_riego = []
 
     def crear_plano(self, tipo: str) -> None:
-        pass
+        plano = []
 
-    def plantar(
-        self, codigo_cultivo: int, coordenadas: list, alto: int, ancho: int
-    ) -> None:
-        pass
+        for i in range(self.alto):
+            fila = []
+
+            for j in range(self.ancho):
+                if tipo == "normal":
+                    fila.append("X")
+                elif tipo == "riego":
+                    fila.append(0)
+
+            plano.append(fila)
+        if tipo == "normal":
+            self.plano = plano
+        elif tipo == "riego":
+            self.plano_riego = plano
+
+    def plantar(self, codigo_cultivo: int, coordenadas: list, alto: int, ancho: int) -> None:
+        fila_inicio, columna_inicio = coordenadas
+
+        for i in range(alto):
+            for j in range(ancho):
+                self.plano[fila_inicio+i][columna_inicio+j] = codigo_cultivo
 
     def regar(self, coordenadas: list, area: int) -> None:
-        pass
+        fila_centro, columna_centro = coordenadas
+
+        radio = area
+        lado = 2 * radio
+
+        fila_min = fila_centro - radio
+        fila_max = fila_centro + radio
+        columna_min = columna_centro - radio
+        columna_max = columna_centro + radio
+
+        # Crear un conjunto de coordenadas para las esquinas
+        esquinas = {
+            (fila_min, columna_min),
+            (fila_min, columna_max),
+            (fila_max, columna_min),
+            (fila_max, columna_max)
+        }
+
+        # Iterar sobre cada celda en el plano de riego
+        for i in range(self.alto):
+            for j in range(self.ancho):
+                # Verificar si la celda está dentro del área del cuadrado
+                if fila_min <= i <= fila_max and columna_min <= j <= columna_max:
+                    # Verificar si la celda no está en las esquinas
+                    if (i, j) not in esquinas:
+                        self.plano_riego[i][j] += 1
+
 
     def eliminar_cultivo(self, codigo_cultivo: int) -> int:
-        pass
+        celda_eliminado = 0
+
+        for i in range(self.alto):
+            for j in range(self.ancho):
+                if self.plano[i][j] == codigo_cultivo:
+                    self.plano[i][j] = "X"
+                    celda_eliminado += 1
+
+        return celda_eliminado
 
 
 class DCCultivo:
@@ -28,13 +78,67 @@ class DCCultivo:
         self.predios = []
 
     def crear_predios(self, nombre_archivo: str) -> str:
-        pass
+        # Verificar si el archivo existe
+        if not os.path.exists("data\\"+nombre_archivo):
+            return "Fallo en la carga de DCCultivo"
+
+
+        with open("data\\"+nombre_archivo, "r") as archivo:
+
+            lineas = archivo.readlines()
+            if not lineas:
+                return "Fallo en la carga de DCCultivo"
+
+            self.predios = []
+
+            for linea in lineas:
+                codigo, alto, ancho = linea.strip().split(",")
+                predio = Predio(codigo, int(alto), int(ancho))
+                predio.crear_plano("normal")
+                predio.crear_plano("riego")
+                self.predios.append(predio)
+
+        return "Predios de DCCultivo cargados exitosamente"
 
     def buscar_y_plantar(self, codigo_cultivo: int, alto: int, ancho: int) -> bool:
-        pass
+        for predio in self.predios:
+            for fila in range(predio.alto - alto +1):
+                for columna in range(predio.ancho - ancho +1):
+                    espace_suficiente = True
+
+                    for i in range(alto):
+                        for j in range(ancho):
+                            if predio.plano[fila + i][columna + j] != "X":
+                                espace_suficiente = False
+                                break
+                            if not espace_suficiente:
+                                break
+
+                    if espace_suficiente:
+                        predio.plantar(codigo_cultivo, [fila, columna], alto, ancho)
+                        return True
+
+        return False
+
 
     def buscar_y_regar(self, codigo_predio: str, coordenadas: list, area: int) -> None:
-        pass
+        for predio in self.predios:
+            if predio.codigo_predio == codigo_predio:
+                predio.regar(coordenadas, area)
+                break
 
     def detectar_plagas(self, lista_plagas: list[list]) -> list[list]:
-        pass
+        afectados = []
+
+        for codigo, coordenadas in lista_plagas:
+            for predio in self.predios:
+                if predio.codigo_predio == codigo:
+                    celdas_eliminadas = predio.eliminar_cultivo(coordenadas)
+                    if celdas_eliminadas > 0:
+                        afectados.append([codigo, celdas_eliminadas])
+                    break
+
+        afectados.sort(key=lambda x: (x[1], x[0]))
+
+        return afectados
+
---
+++ Tareas/T1/main.py
@@ -0,0 +1,88 @@
+from dccultivo import DCCultivo, Predio
+import utils
+
+def menu_inicio():
+    print("¡Bienvenido a DCCultivo!")
+    print("*** Menú de Inicio ***")
+    print("[1] Crear predios")
+    print("[2] Salir del programa")
+    opcion = input("Indique su opción (1, 2): ")
+    return opcion
+
+def menu_acciones():
+    print("*** Menú de Acciones ***")
+    print("[1] Visualizar predio")
+    print("[2] Plantar")
+    print("[3] Regar")
+    print("[4] Buscar y eliminar plagas")
+    print("[5] Salir del programa")
+    opcion = input("Indique su opción (1, 2, 3, 4, 5): ")
+    return opcion
+
+def inicia_programa():
+    dccultivo = DCCultivo()
+    while True:
+        opcion_inicio = menu_inicio()
+
+        if opcion_inicio == "1":
+            nombre_archivo = input("Ingrese el nombre del archivo de predios: ")
+            mensaje = dccultivo.crear_predios(nombre_archivo)
+            print(mensaje)
+            if mensaje == "Predios de DCCultivo cargados exitosamente":
+                while True:
+                    opcion_accion = menu_acciones()
+
+                    if opcion_accion == "1":
+                        predio = None
+                        for i in dccultivo.predios:
+                            if i.codigo_predio == codigo_predio:
+                                predio = i
+                                break
+                        if predio:
+                            utils.imprimir_planos(predio)
+                        else:
+                            print(f"El predio con código {codigo_predio} no existe.")
+
+                    elif opcion_accion == "2":
+                        codigo_cultivo = int(input("Ingrese el código del cultivo: "))
+                        alto = int(input("Ingrese el alto del bloque: "))
+                        ancho = int(input("Ingrese el ancho del bloque: "))
+                        exito = dccultivo.buscar_y_plantar(codigo_cultivo, alto, ancho)
+                        if exito:
+                            print("Cultivo plantado exitosamente.")
+                        else:
+                            print("No se pudo plantar el cultivo en ningún predio.")
+
+                    elif opcion_accion == "3":
+                        codigo_predio = input("Ingrese el código del predio: ")
+                        fila = int(input("Ingrese la fila de la coordenada: "))
+                        columna = int(input("Ingrese la columna de la coordenada: "))
+                        area = int(input("Ingrese el área de riego: "))
+                        dccultivo.buscar_y_regar(codigo_predio, [fila, columna], area)
+                        print("Riego aplicado exitosamente.")
+
+                    elif opcion_accion == "4":
+                        plagas_lista = utils.plagas()
+                        resultado = dccultivo.detectar_plagas(plagas_lista)
+                        print("Resultado de la eliminación de plagas:", resultado)
+
+                    elif opcion_accion == "5":
+                        print("Saliendo del programa...")
+                        break
+                    else:
+                        print("Opción no válida. Inténtelo de nuevo.")
+
+            else:
+                continue
+
+        elif opcion_inicio == "2":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Inténtelo de nuevo.")
+
+
+if __name__ == "__main__":
+    inicia_programa()
+
+
